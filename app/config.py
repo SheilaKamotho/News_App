71bfa15ce9912e182fa43f05df4c257bc518d356
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-13 02:46:30 +0300

    add class

--- app/config.py
+++ app/config.py
@@ -2,9 +2,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything{}?q=bitcoin&apiKey={}'
-
-
 
 class ProdConfig(Config):
     '''
--- app/request.py
+++ app/request.py
@@ -2,3 +2,6 @@ from app import app
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+sources = api_key.get_sources()
+all_articles = api_key.get_everything(q='bitcoin',sources='bbc-news,the-verge', domains='bbc.co.uk,techcrunch.com',from_param='2017-12-01',to='2017-12-12',language='en',sort_by='relevancy',page=2)
