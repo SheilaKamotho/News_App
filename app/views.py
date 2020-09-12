ee5b42280e0be0e33f7db69a45c49ee298fb3b11
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-12 13:53:30 +0300

    add article and source dynamic routes

--- app/views.py
+++ app/views.py
@@ -10,3 +10,19 @@ def index():
     '''
     heading = 'Welcome to News App'
     return render_template('index.html', heading = heading)
+
+@app.route('/source/<news_source>')
+def source(news_source):
+
+    '''
+    View source page function and returns the news source page and its data
+    '''
+    return render_template('source.html',id = news_source)
+
+@app.route('/article/<news_article>')
+def article(news_article):
+
+    '''
+    View article page function and returns the news article page and its data
+    '''
+    return render_template('article.html',id = news_article)
