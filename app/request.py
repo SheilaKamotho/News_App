eef6a381ab46a0c0c1e2e3cb0643297c9fb9f786
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-13 15:48:00 +0300

    add template code for articles

--- app/request.py
+++ app/request.py
@@ -1,13 +1,16 @@
-from app import app
-import urllib.request,json
-from .models import article,source
-from newsapi import NewsApiClient
+# from app import app
+# import urllib.request,json
+# from .models import article,source
+# from newsapi import NewsApiClient
 
-Article = article.Article
-Source = source.Source
+# Article = article.Article
+# Source = source.Source
 
-# Getting api key
-api_key = app.config['NEWS_API_KEY']
+# # Getting api key
+# api_key = app.config['NEWS_API_KEY']
 
-sources = api_key.get_sources()
-all_articles = api_key.get_everything(q='bitcoin',sources='bbc-news,the-verge', domains='bbc.co.uk,techcrunch.com',from_param='2017-12-01',to='2017-12-12',language='en',sort_by='relevancy',page=2)
+# # Getting the movie base url
+# base_url = app.config["NEWS_API_BASE_URL"]
+
+# sources = api_key.get_sources()
+# all_articles = api_key.get_everything(q='bitcoin',sources='bbc-news,the-verge', domains='bbc.co.uk,techcrunch.com',from_param='2017-12-01',to='2017-12-12',language='en',sort_by='relevancy',page=2)
--- app/views.py
+++ app/views.py
@@ -1,15 +1,18 @@
 from flask import render_template
 from app import app
+from newsapi import NewsApiClient
 
-# Views
 @app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
+
     title = 'Welcome to the best News App'
     heading = 'Welcome to News App'
+    
+ 
     return render_template('index.html', heading = heading, title = title)
 
 @app.route('/source/<news_source>')
@@ -19,6 +22,7 @@ def source(news_source):
     View source page function and returns the news source page and its data
     '''
     title = 'News Source'
+
     return render_template('source.html',id = news_source, title = title)
 
 @app.route('/article/<news_article>')
@@ -28,4 +32,33 @@ def article(news_article):
     View article page function and returns the news article page and its data
     '''
     title = 'News Article'
-    return render_template('article.html',id = news_article, title = title)
+    newsapi = NewsApiClient(app.config['NEWS_API_KEY'])
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english,the-verge, bbc-news" )
+ 
+ 
+    articles = topheadlines['articles']
+    
+    desc = []
+    url = []
+    news = []
+    img = []
+    pub = []
+    auth = []
+ 
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+ 
+        news.append(myarticles['title'])
+        url.append(myarticles['url'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        pub.append(myarticles['publishedAt'])
+        auth.append(myarticles['author'])
+ 
+ 
+ 
+    mylist = zip(news, desc,url, img, pub, auth)
+ 
+    return render_template('article.html',id = news_article, title = title, context = mylist)
