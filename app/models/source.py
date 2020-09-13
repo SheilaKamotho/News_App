be1b06e1809dc3f25d4f4e22af4117cb9ce889f9
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-13 03:30:24 +0300

    add article class

---
+++ app/models/source.py
@@ -0,0 +1,13 @@
+class Article:
+    '''
+    Article class to define Article objects
+    '''
+    
+    def __init__(self,name,url,description):
+        self.name = name
+        self.url = url
+        self.description = description
+        
+
+
+
