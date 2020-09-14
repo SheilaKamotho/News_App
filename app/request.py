ce767d0ed8214d685f148ea942cb1e94ee25763a
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-14 06:11:30 +0300

    add nav bar

--- app/request.py
+++ app/request.py
@@ -9,8 +9,7 @@
 # # Getting api key
 # api_key = app.config['NEWS_API_KEY']
 
-# # Getting the movie base url
-# base_url = app.config["NEWS_API_BASE_URL"]
+# # # Getting the movie base url
+# # base_url = app.config["NEWS_API_BASE_URL"]
 
-# sources = api_key.get_sources()
-# all_articles = api_key.get_everything(q='bitcoin',sources='bbc-news,the-verge', domains='bbc.co.uk,techcrunch.com',from_param='2017-12-01',to='2017-12-12',language='en',sort_by='relevancy',page=2)
+# all_articles = api_key.get_everything(sort_by='source')
