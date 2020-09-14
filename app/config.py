764e69bd02fd75861c5dd9bf4235c4b6750f03bf
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-14 16:43:41 +0300

    register blueprint

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 class ProdConfig(Config):
     '''
     Production  configuration child class
@@ -22,3 +24,4 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+config_options = {'development':DevConfig,'production':ProdConfig}
