a8b0f4dbef2b7a80cefc924691fa49e01b6b4f0f
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-15 08:39:26 +0300

    add styling

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    NEWS_API_KEYS = os.environ.get('NEWS_API_KEY')
-    NEWS_API_KEY= 'd967ec379de94378b37409a2aa549775'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
 class ProdConfig(Config):
     '''
     Production  configuration child class
