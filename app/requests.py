c17dbbb196f073dae0672c303dfa8cdd1f2b07e1
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-16 11:19:51 +0300

    add deployment changes

--- app/requests.py
+++ app/requests.py
@@ -1,16 +1,16 @@
-import urllib.request,json
-from .models import Article,Source
+# import urllib.request,json
+# from .models import Article,Source
 
 
-# Getting api key
-api_key = None
+# # Getting api key
+# api_key = None
 
-# Getting the movie base url
-base_url = None
+# # Getting the movie base url
+# base_url = None
 
-# all_articles = api_key.get_everything(sort_by='source')
+# # all_articles = api_key.get_everything(sort_by='source')
 
-def configure_request(app):
-    global api_key,base_url
-    api_key = app.config['NEWS_API_KEY']
-    base_url = app.config['NEWS_API_BASE_URL']
+# def configure_request(app):
+#     global api_key,base_url
+#     api_key = app.config['NEWS_API_KEY']
+#     base_url = app.config['NEWS_API_BASE_URL']
