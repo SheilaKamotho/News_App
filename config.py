4eb09899dc318c1a29429d7862d3909285c8362c
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-16 12:10:09 +0300

    add deployment changes

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    # NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     
 class ProdConfig(Config):
     '''
